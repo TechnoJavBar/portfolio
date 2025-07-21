df0c608e10dbb34c1b554ec45d7f5515ad9ff3ef
xabarjim442 <javibarrancajz@gmail.com>
2025-07-21 20:54:29 +0200

    se ha importado el elemento navegator

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,15 @@
 import './header.css';
+import { Navegator } from './navegator';
 
 export function Header() {
   return (
     <header className='header'>
-      <h1>Mi Portfolio</h1>
+        <div>
+            <h1>Mi Portfolio</h1>  
+        </div>
+        <div>
+            <Navegator />
+        </div>
     </header>
   )
 }
