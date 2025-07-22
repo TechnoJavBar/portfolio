b2408db433f3303baf11039bb484be21e28bf1e0
xabarjim442 <javibarrancajz@gmail.com>
2025-07-22 22:26:29 +0200

    se ha cambiado la funcion y se ha añadido codigo html

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 import {Header} from './components/header.jsx'
+import {Presentation} from './components/presentation.jsx'
 
 function App() {
   return (
-    <main>
+    <main className="main">
       <Header />
-      <h1>Hola desde mi portfolio</h1>
+      <aside className='aside'>
+        <ul>
+          <li>Link 1</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+        </ul>
+      </aside>
+      <section className='section'>
+        <Presentation />
+      </section>
     </main>
   )
 }
