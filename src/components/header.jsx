57965eae1ff4c9534a67dc86a868175e01e27fbd
xabarjim442 <javibarrancajz@gmail.com>
2025-07-22 22:25:02 +0200

    se ha eliminado el div que contenia un h1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,12 +4,9 @@ import { Navegator } from './navegator';
 export function Header() {
   return (
     <header className='header'>
-        <div>
-            <h1>Mi Portfolio</h1>  
-        </div>
-        <div>
-            <Navegator />
-        </div>
+      <div>
+          <Navegator />
+      </div>
     </header>
   )
 }
