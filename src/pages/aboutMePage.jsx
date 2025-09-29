29e450c57630c6a59d696e34adb89d7746291856
xabarjim442 <javibarrancajz@gmail.com>
2025-09-29 10:43:26 +0200

    se ha añadido una clase de css

--- src/pages/aboutMePage.jsx
+++ src/pages/aboutMePage.jsx
@@ -1,10 +1,11 @@
 import {ImagenProfile} from "../components/imagenProfile.jsx";
 import profilePic from "../assets/user.jpeg";
 import {AboutMe} from "../components/aboutMe.jsx";
+import "../styles/aboutMePage.css";
 
 export function AboutMePage(){
     return(
-        <main>
+        <main className="aboutMePage">
             <ImagenProfile imagen={profilePic} alt="Foto de perfil"/>
             <AboutMe />
         </main>
