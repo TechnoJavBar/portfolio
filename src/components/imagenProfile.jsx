4cdaafea829f510236aaaf3ffe4babe65f482d2c
xabarjim442 <javibarrancajz@gmail.com>
2025-09-29 11:21:29 +0200

    se h aañadido framer motion

--- src/components/imagenProfile.jsx
+++ src/components/imagenProfile.jsx
@@ -1,9 +1,23 @@
 import './imagenProfile.css'
+import {motion} from 'framer-motion'
 
 export function ImagenProfile({imagen, alt}) {
     return (
-        <div className="presentacionImg">
+        <motion.div 
+        className="presentacionImg"
+        initial={{
+            opacity: 0,
+            scale: 0.5,
+            x: 100
+        }}
+        whileInView={{
+            opacity: 1,
+            scale: 1,
+            x: 0,
+            transition: { duration: 0.5, delay: 0.2, ease: 'easeInOut' }
+        }}
+        >
             <img src={imagen} alt={alt}></img>
-        </div>
+        </motion.div>
     )
 }
