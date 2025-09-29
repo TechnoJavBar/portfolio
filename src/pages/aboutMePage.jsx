70fc92ea5d94a289aa50f4443d6a1c00d2530531
xabarjim442 <javibarrancajz@gmail.com>
2025-09-29 10:02:36 +0200

    se han añadido dos componentes

--- src/pages/aboutMePage.jsx
+++ src/pages/aboutMePage.jsx
@@ -1,6 +1,12 @@
+import {ImagenProfile} from "../components/imagenProfile.jsx";
+import profilePic from "../assets/user.jpeg";
+import {AboutMe} from "../components/aboutMe.jsx";
+
 export function AboutMePage(){
     return(
         <main>
+            <ImagenProfile imagen={profilePic} alt="Foto de perfil"/>
+            <AboutMe />
         </main>
     );
 }
