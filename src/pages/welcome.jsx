b2fa101275de91d103da0dae5fe64fbfa66b04f4
xabarjim442 <javibarrancajz@gmail.com>
2025-09-29 09:46:38 +0200

    se ha añadido la clase welcome-main

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,10 +1,11 @@
 import { Presentation } from "../components/presentation";
 import { DonwloadCVButton } from "../components/downloadCVButton";
+import "./welcome.css";
 
 export function Welcome(){
 
     return(
-    <main>
+    <main className="welcome-main">
         <Presentation />
         <DonwloadCVButton />
     </main>
