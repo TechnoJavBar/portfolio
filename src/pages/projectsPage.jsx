e4edbf726c2235444a91271d1fd971535cadb18f
xabarjim442 <javibarrancajz@gmail.com>
2025-09-29 09:29:46 +0200

    se ha creado la pagina

---
+++ src/pages/projectsPage.jsx
@@ -0,0 +1,7 @@
+export function ProjectsPage(){
+    return(
+        <main>
+            
+        </main>
+    );
+}
---
+++ src/pages/skillsPage.jsx
@@ -0,0 +1,7 @@
+export function SkillsPage(){
+    return(
+        <main>
+
+        </main>
+    );
+}
