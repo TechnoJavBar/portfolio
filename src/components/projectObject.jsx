ff7aa183bd6214ad8862e5a87c1c9a9ae34c5e5f
xabarjim442 <javibarrancajz@gmail.com>
2025-10-02 14:06:03 +0200

    Se ha creado el componente ProjectObject

---
+++ src/components/projectObject.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./projectObject.css";
+import { FaGithub } from "react-icons/fa";
+import {motion} from "framer-motion";
+
+export function ProjectObject({image, title, description, link1, link2}){
+    return(
+        <motion.div 
+        className="projectObject"
+        whileHover={{
+            scale: 1.05,
+            transition: { duration: 0.3 },
+        }}
+        >
+            <img src={image} alt={title}/>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <Link to={link1} className="link">Ver Proyecto</Link>
+            <Link to={link2} className="link"><FaGithub style={{width:"20px", height:"auto", color:"#ffff"}}/> Ver Código</Link>
+        </motion.div>
+    );
+}
