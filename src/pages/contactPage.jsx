5088372bef86996d6e49cad1ce85ed0039a868e3
xabarjim442 <javibarrancajz@gmail.com>
2025-10-10 09:41:41 +0200

    se ha añadido el formulario de contacto

--- src/pages/contactPage.jsx
+++ src/pages/contactPage.jsx
@@ -1,7 +1,9 @@
+import { ContactForm } from "../components/contactForm";
+
 export function ContactPage(){
     return(
         <main>
-            
+           <ContactForm/> 
         </main>
     );
 }
