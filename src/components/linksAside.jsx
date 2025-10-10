b33ad80df74f2ae23139efd2a9d3bed0db521760
xabarjim442 <javibarrancajz@gmail.com>
2025-10-10 08:34:23 +0200

    se ha añadido framer motion para darle una animacion  nua mejor estetica

--- src/components/linksAside.jsx
+++ src/components/linksAside.jsx
@@ -1,16 +1,26 @@
 import {FaGithub, FaInstagram, FaLinkedin, FaFacebook } from 'react-icons/fa'
+import { motion } from 'framer-motion';
 import './linksAside.css'
 
 export function LinksAside() 
 {
     return(
-        <aside className='aside'>
+        <motion.aside className='aside'
+        initial={{
+          opacity: 0.2
+        }}
+        whileHover={{
+          opacity: 1,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          transition: { duration: 0.3 }
+        }}
+        >
         <ul>
-          <li><a href='#'><FaGithub className='FaGithub'/></a></li>
-          <li><a href='#'><FaLinkedin className='FaLinkedin'/></a></li>
+          <li><a href='https://github.com/TechnoJavBar'><FaGithub className='FaGithub'/></a></li>
+           <li><a href='#'><FaLinkedin className='FaLinkedin'/></a></li>{/*www.linkedin.com/in/antonio-javier-barranca-jiménez-064408307 */}
           <li><a href='#'><FaInstagram className='FaInstagram' /></a></li>
-          <li><a href='#'><FaFacebook className='FaFacebook'/></a></li>
+          <li><a href='https://www.facebook.com/profile.php?id=61573184274798'><FaFacebook className='FaFacebook'/></a></li>
         </ul>
-      </aside>
+      </motion.aside>
     );
 }
