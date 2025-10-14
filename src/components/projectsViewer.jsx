945eb86518324473826039e63aa1753f080cbf40
xabarjim442 <javibarrancajz@gmail.com>
2025-10-14 13:52:29 +0200

    se ha añadido una llamada a la api de github para mostrar todos los proyectos

--- src/components/projectsViewer.jsx
+++ src/components/projectsViewer.jsx
@@ -1,24 +1,36 @@
 import "./projectsViewer.css";
 import { ProjectObject } from "./projectObject";
 import interfazBSP from "../assets/interfazBSP.jpg";
+import {useState, useEffect} from "react"
+import { image } from "framer-motion/client";
+
 export function ProjectsViewer(){
+    const [repos, setRepos]  = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+
+        fetch("https://api.github.com/users/TechnoJavBar/repos")
+        .then((res) => res.json())
+        .then((data) => {
+            const filtered  = data.filter(
+                // (repos) => !repos.fork && repos.description
+                 (repos) => !repos.fork
+            );
+            setRepos(filtered);
+        })
+        .catch((err) => console.log("error al cargar el repositorio: ", err))
+        .finally(()=> setLoading(false));
+    }, []);
+
+    if ( loading ) return(<p>Cargando Proyectos...</p>);
     return(
         <section className="projectsViewer">
             <h1>Mis Proyectos</h1>{/*TODO: cambiar color h1*/}
             <div className="container-projects">
-                <ProjectObject
-                    image={interfazBSP}
-                    title={"BarrancaSaludPlus"}
-                    description={"Aplicación para la gestión de un centro de salud privado."}
-                    link1={""}
-                    link2={"/projects/portafolio"}/>
-                <ProjectObject
-                    image={interfazBSP}
-                    title={"MariaFlorDeJara"}
-                    description={"Catálogo online de diferentes productos."}
-                    link1={""}
-                    link2={"https://github.com/TechnoJavBar/frontendMariaFlorDeJara/tree/main/client"}
-                />
+                {repos.map((repo) =>(
+                    <ProjectObject key={repo.id} image="" title={repo.name} description={repo.description || "sin descripcion"} link1={""} link2={repo.html_url}/>
+                ))}
             </div>
         </section>
     );
