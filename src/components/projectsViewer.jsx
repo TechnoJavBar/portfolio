11e172e7cbdb545444e202d19ce43ddf04b32b4b
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 16:19:34 +0100

    Se ha mejorado el manejo de la carga de proyectos con un spinner animado.

--- src/components/projectsViewer.jsx
+++ src/components/projectsViewer.jsx
@@ -3,6 +3,7 @@ import { ProjectObject } from "./projectObject";
 import interfazBSP from "../assets/interfazBSP.jpg";
 import { useState, useEffect } from "react";
 import { image } from "framer-motion/client";
+import { motion } from "framer-motion";
 
 export function ProjectsViewer() {
   const [repos, setRepos] = useState([]);
@@ -30,7 +31,21 @@ export function ProjectsViewer() {
     fetchRepos();
   }, []);
 
-  if (loading) return <p>Cargando Proyectos...</p>;
+  if (loading) {
+    return (
+      <div className="container-spinner">
+        <motion.div
+          className="spinner"
+          animate={{ rotate: 360 }}
+          transition={{
+            duration: 1.5,
+            repeat: Infinity,
+            ease: "linear",
+          }}
+        />
+      </div>
+    );
+  }
   return (
     <section className="projectsViewer">
       <h1>Mis Proyectos</h1>
