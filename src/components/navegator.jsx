4c8595ab6d29e0a1e2a39a9b35a4d3aae51039c8
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 17:26:21 +0100

    Se ha añadido animación a los elementos del menú en el componente Navegator.

--- src/components/navegator.jsx
+++ src/components/navegator.jsx
@@ -1,16 +1,79 @@
-import './navegator.css';
-import { Link } from 'react-router-dom';
+import "./navegator.css";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { linearGradient } from "framer-motion/client";
+
+const buttonsVariants = {
+  hover: {
+    scale: 1.1,
+    transition: {
+      duration: 0.3,
+      ease: "easeInOut",
+    },
+  },
+
+  tap: {
+    scale: 0.9,
+    transition: {
+      duration: 0.2,
+    },
+  },
+};
 
 export function Navegator() {
-    return (
-        <nav className="navegator">
-            <ul>
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/about">Sobre mí</Link></li>
-                <li><Link to="/skills">Habilidades</Link></li>
-                <li><Link to="/projects">Proyectos</Link></li>
-                <li><Link to="/contact">Contacto</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    // <nav className="navegator">
+    //     <ul>
+    //         <li><Link to="/">Inicio</Link></li>
+    //         <li><Link to="/about">Sobre mí</Link></li>
+    //         <li><Link to="/skills">Habilidades</Link></li>
+    //         <li><Link to="/projects">Proyectos</Link></li>
+    //         <li><Link to="/contact">Contacto</Link></li>
+    //     </ul>
+    // </nav>
+    <nav className="navegator">
+      <ul>
+        <motion.li
+          variants={buttonsVariants}
+          whileHover="hover"
+          whileTap="tap"
+          className="nav-item"
+        >
+          <Link to="/">Inicio</Link>
+        </motion.li>
+        <motion.li
+          variants={buttonsVariants}
+          whileHover="hover"
+          whileTap="tap"
+          className="nav-item"
+        >
+          <Link to="/about">Sobre mí</Link>
+        </motion.li>
+        <motion.li
+          variants={buttonsVariants}
+          whileHover="hover"
+          whileTap="tap"
+          className="nav-item"
+        >
+          <Link to="/skills">Habilidades</Link>
+        </motion.li>
+        <motion.li
+          variants={buttonsVariants}
+          whileHover="hover"
+          whileTap="tap"
+          className="nav-item"
+        >
+          <Link to="/projects">Proyectos</Link>
+        </motion.li>
+        <motion.li
+          variants={buttonsVariants}
+          whileHover="hover"
+          whileTap="tap"
+          className="nav-item"
+        >
+          <Link to="/contact">Contacto</Link>
+        </motion.li>
+      </ul>
+    </nav>
+  );
 }
