431c5c6f8a182b01301cec5343fe51f3938554f6
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 22:04:44 +0100

    se ha modificado el nombre debido a un error de escritura

--- src/pages/skillsPage.jsx
+++ src/pages/skillsPage.jsx
@@ -1,9 +1,9 @@
 import { Skills } from "../components/Skills.jsx";
 
-export function SkillsPage(){
-    return(
-        <main>
-            <Skills />
-        </main>
-    );
+export function SkillsPage() {
+  return (
+    <main>
+      <Skills />
+    </main>
+  );
 }
