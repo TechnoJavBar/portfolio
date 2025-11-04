ef50d931170687d4a53af43217d6ccd9d88d63ee
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 13:00:31 +0100

    Se ha introducido el componente donwloadCVButton

--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -1,16 +1,21 @@
-import './presentation.css'
-import profilePhoto from '../assets/fotoAntonioJavierBarrancajimenez.png'
-import { ImagenProfile } from './imagenProfile'
+import "./presentation.css";
+import profilePhoto from "../assets/fotoAntonioJavierBarrancajimenez.png";
+import { ImagenProfile } from "./imagenProfile";
+import { DonwloadCVButton } from "../components/downloadCVButton";
 export function Presentation() {
-    return (
-        <div className="presentacion">
-            <div className="presentacionInfo">
-                <h1>Antonio Javier Barranca Jimenez</h1>
-                <p>Desarrollador aplicaciones web</p>
-            </div>
-            <div>
-                <ImagenProfile imagen={profilePhoto} alt="Foto de perfil Antonio Barranca" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="presentacion">
+      <div className="presentacionInfo">
+        <h1>Antonio Javier Barranca Jimenez</h1>
+        <p>Desarrollador aplicaciones web</p>
+        <DonwloadCVButton />
+      </div>
+      <div>
+        <ImagenProfile
+          imagen={profilePhoto}
+          alt="Foto de perfil Antonio Barranca"
+        />
+      </div>
+    </div>
+  );
 }
