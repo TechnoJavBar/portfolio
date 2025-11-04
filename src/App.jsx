995aba5527309370349281c9051ef30c5745f9b6
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 18:24:00 +0100

    Se ha mejorado la consistencia del código en el componente App, ajustando el formato de importaciones y la estructura de las rutas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import './App.css'
-import {Header} from './components/header.jsx'
-import { Routes, Route } from 'react-router-dom'
-import {Welcome} from './pages/welcome.jsx'
-import {AboutMePage} from './pages/aboutMePage.jsx'
-import { SkillsPage } from './pages/skillsPage.jsx'
-import { ProjectsPage } from './pages/projectsPage.jsx'
-import { ContactPage } from './pages/contactPage.jsx'
-import { LinksAside } from './components/linksAside.jsx'
+import "./App.css";
+import { Header } from "./components/header.jsx";
+import { Routes, Route } from "react-router-dom";
+import { Welcome } from "./pages/welcome.jsx";
+import { AboutMePage } from "./pages/aboutMePage.jsx";
+import { SkillsPage } from "./pages/skillsPage.jsx";
+import { ProjectsPage } from "./pages/projectsPage.jsx";
+import { ContactPage } from "./pages/contactPage.jsx";
 
 function App() {
   return (
     <main className="main">
       <Header />
       <Routes>
-        <Route path='/' element={<Welcome/>}/>
-        <Route path='/about' element={<AboutMePage/>}/>
-        <Route path='/skills' element={<SkillsPage/>}/>
-        <Route path='/projects' element={<ProjectsPage/>}/>
-        <Route path='/contact' element={<ContactPage/>}/>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/about" element={<AboutMePage />} />
+        <Route path="/skills" element={<SkillsPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      <LinksAside />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
