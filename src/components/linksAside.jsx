9ff9ab1da80530d65921b4f76297b340e01560fd
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 18:24:33 +0100

    Se ha mejorado la consistencia del código en el componente LinksAside, ajustando el formato de importaciones y añadiendo efectos de hover.

--- src/components/linksAside.jsx
+++ src/components/linksAside.jsx
@@ -1,26 +1,38 @@
-import {FaGithub, FaInstagram, FaLinkedin, FaFacebook } from 'react-icons/fa'
-import { motion } from 'framer-motion';
-import './linksAside.css'
+import { FaGithub, FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { motion } from "framer-motion";
+import "./linksAside.css";
 
-export function LinksAside() 
-{
-    return(
-        <motion.aside className='aside'
-        initial={{
-          opacity: 0.2
-        }}
-        whileHover={{
-          opacity: 1,
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          transition: { duration: 0.3 }
-        }}
-        >
-        <ul>
-          <li><a href='https://github.com/TechnoJavBar'><FaGithub className='FaGithub'/></a></li>
-           <li><a href='#'><FaLinkedin className='FaLinkedin'/></a></li>{/*www.linkedin.com/in/antonio-javier-barranca-jiménez-064408307 */}
-          <li><a href='#'><FaInstagram className='FaInstagram' /></a></li>
-          <li><a href='https://www.facebook.com/profile.php?id=61573184274798'><FaFacebook className='FaFacebook'/></a></li>
-        </ul>
-      </motion.aside>
-    );
+export function LinksAside() {
+  return (
+    <motion.aside
+      className="aside"
+      initial={{
+        opacity: 0.2,
+      }}
+    >
+      <ul>
+        <li>
+          <a href="https://github.com/TechnoJavBar">
+            <FaGithub className="FaGithub" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FaLinkedin className="FaLinkedin" />
+          </a>
+        </li>
+        {/*www.linkedin.com/in/antonio-javier-barranca-jiménez-064408307 */}
+        <li>
+          <a href="#">
+            <FaInstagram className="FaInstagram" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/profile.php?id=61573184274798">
+            <FaFacebook className="FaFacebook" />
+          </a>
+        </li>
+      </ul>
+    </motion.aside>
+  );
 }
