21125cc6406201239eb7a83e9530c9ba503fb2bc
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 19:18:18 +0100

    Se ha reemplazado el componente LinksAside por un menú de enlaces en el componente Header, mejorando la estructura y la presentación de los iconos de redes sociales.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,42 @@
 import "./header.css";
 import { Navegator } from "./navegator";
-import { LinksAside } from "./linksAside";
+import { FaGithub, FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 export function Header() {
   return (
     <header className="header">
       <div>
-        <LinksAside />
+        <motion.aside
+          className="aside"
+          initial={{
+            opacity: 0.2,
+          }}
+        >
+          <ul>
+            <li>
+              <a href="https://github.com/TechnoJavBar">
+                <FaGithub className="FaGithub" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaLinkedin className="FaLinkedin" />
+              </a>
+            </li>
+            {/*www.linkedin.com/in/antonio-javier-barranca-jiménez-064408307 */}
+            <li>
+              <a href="#">
+                <FaInstagram className="FaInstagram" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/profile.php?id=61573184274798">
+                <FaFacebook className="FaFacebook" />
+              </a>
+            </li>
+          </ul>
+        </motion.aside>
         <Navegator />
       </div>
     </header>
