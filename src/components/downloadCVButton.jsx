5af43777e80114cf1d10bbdda2a648f26ca56e12
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 13:00:53 +0100

    Se han esctito comentarios para un mejor entendimiento

--- src/components/downloadCVButton.jsx
+++ src/components/downloadCVButton.jsx
@@ -1,46 +1,45 @@
 import "./downloadCVButton.css";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { MdFileDownload } from "react-icons/md";
 
 export function DonwloadCVButton() {
-    const spanVariants = {
-        initial: { 
-            opacity: 0, 
-            x: -10 
-        },
-        hover: { 
-            opacity: 1, 
-            x: 0 
-        }
-    };
+  //Motion Values
+  const spanVariants = {
+    initial: {
+      opacity: 0,
+      x: -10,
+    },
+    hover: {
+      opacity: 1,
+      x: 0,
+    },
+  };
 
-    const buttonVariants = {
-        initial: { 
-            scale: 1 
-        },
-        hover: { 
-            scale: 1.1, 
-            borderRadius: "50px" 
-        },
-        tap: { 
-            scale: 0.9 
-        }
-    };
+  const buttonVariants = {
+    initial: {
+      scale: 1,
+    },
+    hover: {
+      scale: 1.1,
+      borderRadius: "50px",
+    },
+    tap: {
+      scale: 0.9,
+    },
+  };
 
-    return(
-        <motion.button
-        className="download-cv-button"
-        variants={buttonVariants}
-        initial="initial"
-        whileHover="hover"
-        whileTap="tap"
-        >
-            <motion.span
-            variants={spanVariants}
-            >
-                <MdFileDownload />
-            </motion.span>
-            descargar CV
-        </motion.button>
-    );
+  return (
+    <motion.button
+      className="download-cv-button"
+      variants={buttonVariants}
+      initial="initial"
+      whileHover="hover"
+      whileTap="tap"
+    >
+      <motion.span variants={spanVariants}>
+        <MdFileDownload />
+      </motion.span>
+      descargar CV
+    </motion.button>
+  );
 }
