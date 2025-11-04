740e02387cbcd8fa627a392af7cce1d4dbdc1e2f
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 16:15:32 +0100

    Se ha mejorado el fetch a la api

--- src/components/projectsViewer.jsx
+++ src/components/projectsViewer.jsx
@@ -1,37 +1,52 @@
 import "./projectsViewer.css";
 import { ProjectObject } from "./projectObject";
 import interfazBSP from "../assets/interfazBSP.jpg";
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 import { image } from "framer-motion/client";
 
-export function ProjectsViewer(){
-    const [repos, setRepos]  = useState([]);
-    const [loading, setLoading] = useState(true);
+export function ProjectsViewer() {
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
+  useEffect(() => {
+    async function fetchRepos() {
+      try {
+        const request = await fetch(
+          "https://api.github.com/users/TechnoJavBar/repos"
+        );
 
-        fetch("https://api.github.com/users/TechnoJavBar/repos")
-        .then((res) => res.json())
-        .then((data) => {
-            const filtered  = data.filter(
-                // (repos) => !repos.fork && repos.description
-                 (repos) => !repos.fork
-            );
-            setRepos(filtered);
-        })
-        .catch((err) => console.log("error al cargar el repositorio: ", err))
-        .finally(()=> setLoading(false));
-    }, []);
+        if (!request.ok) throw new Error("Error al cargar los repositorios");
 
-    if ( loading ) return(<p>Cargando Proyectos...</p>);
-    return(
-        <section className="projectsViewer">
-            <h1>Mis Proyectos</h1>{/*TODO: cambiar color h1*/}
-            <div className="container-projects">
-                {repos.map((repo) =>(
-                    <ProjectObject key={repo.id} image="" title={repo.name} description={repo.description || "sin descripcion"} link1={""} link2={repo.html_url}/>
-                ))}
-            </div>
-        </section>
-    );
+        const data = await request.json();
+        const filteredData = data.filter((repo) => !repo.fork);
+        setRepos(filteredData);
+      } catch (err) {
+        console.log("error al cargar el repositorio: ", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchRepos();
+  }, []);
+
+  if (loading) return <p>Cargando Proyectos...</p>;
+  return (
+    <section className="projectsViewer">
+      <h1>Mis Proyectos</h1>
+      {/*TODO: cambiar color h1*/}
+      <div className="container-projects">
+        {repos.map((repo) => (
+          <ProjectObject
+            key={repo.id}
+            image=""
+            title={repo.name}
+            description={repo.description || "sin descripcion"}
+            link1={""}
+            link2={repo.html_url}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
