325c5e32e1e0100cab03594f82cf31eccf94e7e9
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-04 12:59:58 +0100

    Se ha eliminado el componente downloadCVButton

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,13 +1,10 @@
 import { Presentation } from "../components/presentation";
-import { DonwloadCVButton } from "../components/downloadCVButton";
 import "./welcome.css";
 
-export function Welcome(){
-
-    return(
+export function Welcome() {
+  return (
     <main className="welcome-main">
-        <Presentation />
-        <DonwloadCVButton />
+      <Presentation />
     </main>
-    );
+  );
 }
