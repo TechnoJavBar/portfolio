cd1847fe5550fa1d8f33faafaab24abd1054795a
TechnoJavBar <javibarrancajz@gmail.com>
2026-05-05 09:58:59 +0200

    Se ha modificado la estructura del componente añadiendo componentes Link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,28 @@
-import "./header.css";
-import { Navegator } from "./navegator";
+import "./css/header.css";
+import { Link } from "react-router-dom";
+import { ImagenProfile } from "./imagenProfile.jsx";
+import profilePhoto from "../assets/fotoAntonioJavierBarrancajimenez.png";
+import { IoSettingsOutline } from "react-icons/io5";
 
 export function Header() {
   return (
     <header className="header">
-      <div>
-        <Navegator />
-      </div>
+      <nav className="nav">
+        <div className="nav-izda">
+          <ImagenProfile imagen={profilePhoto} src="AJ" />
+          <Link to={"/"}>Inicio</Link>
+          <Link to={"/about"}>Sobre mí</Link>
+          <Link to={"/projects"}>Proyectos</Link>
+          <Link to={"/skills"}>Skills</Link>
+          <Link to={"/contact"}>Contacto</Link>
+        </div>
+        <div className="nav-dcha">
+          <Link to={"/config"}>
+            <IoSettingsOutline />
+          </Link>
+          <h2>Javi TV</h2>
+        </div>
+      </nav>
     </header>
   );
 }
