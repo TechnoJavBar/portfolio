a2a92863f34170595c427003174e78c1460e67b5
TechnoJavBar <javibarrancajz@gmail.com>
2026-05-05 10:06:19 +0200

    Se ha modificado la ruta del css, se ha eliminado codigo inecesario

--- src/components/imagenProfile.jsx
+++ src/components/imagenProfile.jsx
@@ -1,23 +1,15 @@
-import './imagenProfile.css'
+import './css/imagenProfile.css'
 import {motion} from 'framer-motion'
 
 export function ImagenProfile({imagen, alt}) {
     return (
-        <motion.div 
-        className="presentacionImg"
-        initial={{
-            opacity: 0,
-            scale: 0.5,
-            x: 100
-        }}
-        whileInView={{
-            opacity: 1,
-            scale: 1,
-            x: 0,
-            transition: { duration: 0.5, delay: 0.2, ease: 'easeInOut' }
-        }}
+        <motion.div
+            className="nav-profile-wrapper"
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1}}
+            transition={{ duration: 0.3}}
         >
-            <img src={imagen} alt={alt}></img>
+        <img src={imagen} alt={alt} className='nav-profile-img'/>    
         </motion.div>
     )
 }
