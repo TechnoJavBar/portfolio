9f8871ad3d769fedade7987d6b47ad4210db1dbb
TechnoJavBar <javibarrancajz@gmail.com>
2026-05-05 11:04:40 +0200

    se ha establecido la configuración del tamaño de la imagen para que el componente pueda ser reutilizable

--- src/components/imagenProfile.jsx
+++ src/components/imagenProfile.jsx
@@ -1,13 +1,17 @@
 import './css/imagenProfile.css'
 import {motion} from 'framer-motion'
 
-export function ImagenProfile({imagen, alt}) {
+export function ImagenProfile({imagen, alt, px}) {
     return (
         <motion.div
             className="nav-profile-wrapper"
             initial={{ opacity: 0}}
             animate={{ opacity: 1}}
             transition={{ duration: 0.3}}
+            style={{
+                width: px,
+                height: px
+            }}
         >
         <img src={imagen} alt={alt} className='nav-profile-img'/>    
         </motion.div>
