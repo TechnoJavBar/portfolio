d08c235b3d7ea3e07b011b040777cb6e05c748bd
TechnoJavBar <javibarrancajz@gmail.com>
2026-05-05 09:55:14 +0200

    Se han modificado los nombres de las pages importadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import "./App.css";
 import { Header } from "./components/header.jsx";
 import { Routes, Route } from "react-router-dom";
-import { Welcome } from "./pages/welcome.jsx";
+import { WelcomeScreen } from "./pages/welcomeScreen.jsx";
 import { AboutMePage } from "./pages/aboutMePage.jsx";
-import { SkillsPage } from "./pages/skillsPage.jsx";
-import { ProjectsPage } from "./pages/projectsPage.jsx";
-import { ContactPage } from "./pages/contactPage.jsx";
+import { SkillsScreen } from "./pages/skillsScreen.jsx";
+import { ProjectsScreen } from "./pages/projectsScreen.jsx";
+import { ContactScreen } from "./pages/contactScreen.jsx";
+import { ConfigurationScreen } from "./pages/configurationScreen.jsx";
 
 function App() {
   return (
     <main className="main">
       <Header />
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<WelcomeScreen />} />
         <Route path="/about" element={<AboutMePage />} />
-        <Route path="/skills" element={<SkillsPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/skills" element={<SkillsScreen />} />
+        <Route path="/projects" element={<ProjectsScreen />} />
+        <Route path="/contact" element={<ContactScreen />} />
+        <Route path="/config" element={<ConfigurationScreen />} />
       </Routes>
     </main>
   );
